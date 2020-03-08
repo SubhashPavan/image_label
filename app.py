722bac731dc9c86a3457750dd35db62ca011469f
Pavan Subhash Tirumalasetti <pavansubhash_t@infosys.com>
2020-03-08 07:53:52 +0530

    Removed Port

--- app.py
+++ app.py
@@ -75,5 +75,5 @@ def predict_ld():
     return None
 
 if __name__ == '__main__':
-    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server = WSGIServer(('0.0.0.0'), app)
     http_server.serve_forever()        
